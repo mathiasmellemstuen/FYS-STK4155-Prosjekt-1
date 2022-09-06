f020824d67e72a857a64d77e9bc1f8a9ea73b959
erlend1202 <erlendkri1202@gmail.com>
2022-09-06 12:48:36 +0200

    merge

--- FrankeFunction.py
+++ FrankeFunction.py
@@ -1,8 +1,37 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+from random import random, seed
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+# Make data.
+x = np.arange(0, 1, 0.05)
+y = np.arange(0, 1, 0.05)
+x, y = np.meshgrid(x,y)
+
+
+x_noise = np.random.normal(0,1,20)
+y_noise = np.random.normal(0,1,20)
 
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+
     return term1 + term2 + term3 + term4
+
+
+#for loop for permutations
+count = 0
+for i in range(6):
+    for j in range(5-i,-1, -1):
+        if i>0 or j>0:
+            print(i,j)
+            count += 1
+          
+
+
+print(count)
