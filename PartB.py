23e22420e1fa8a1a282972b99369c2644c15c2d8
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-09-06 14:25:16 +0200

    Updated create design matrix.

--- PartB.py
+++ PartB.py
@@ -9,35 +9,31 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 x = np.arange(0, 1, 0.05)
 y = np.arange(0, 1, 0.05)
 
+# Maximum number of polynominals, will also determine size of design matrix
+max_polynominal = 5
+def create_design_matrix(x, y, max_polyniominal = 5):
+    X = np.ones((len(x), max_polyniominal))
+
+    for i in range(len(x)):
+        for j in range(1, max_polyniominal):
+
+            poly_type = (j - 1) % 3
+            current_polynominal = 1 + np.floor(j / 4)
+
+            if poly_type == 0:
+                X[i, j] = (x[i] ** current_polynominal) 
+            elif poly_type == 1:
+                X[i, j] = (y[i] ** current_polynominal) 
+            elif poly_type == 2:
+                X[i, j] = (x[i] ** current_polynominal) * (y[i] ** current_polynominal) 
+    return X
+
+X = create_design_matrix(x, y, 6)
+print(X)
+
 # Creating meshgrid
 x, y = np.meshgrid(x, y)
 z = FrankeFunctionNoised(x, y, 0.05)
-
-# Maximum number of polynominals, will also determine size of design matrix
-max_polynominal = 5
-
-def create_design_matrix(x, y, max_polyniominal = 5):
-    X = np.zeros((len(x), max_polyniominal + 1))
-
-    # Assigning ones at the first row
-    X[:, 0] = np.ones(len(X[:,0]))
-    
-    for i in range(len(x)):
-        for j in range(0, max_polyniominal):
-
-            poly_type = j % 3
-            current_polynominal = 1 + np.floor(j / 3)
-            print(poly_type)
-            if poly_type == 0:
-                X[i, j] = (x[i, 0] ** current_polynominal) 
-            if poly_type == 1:
-                X[i, j] = (y[i, 0] ** current_polynominal) 
-            if poly_type == 2:
-                X[i, j] = (x[i, 0] ** current_polynominal) * (y[i, 0] ** current_polynominal) 
-
-    print(X)
-
-create_design_matrix(x, y)
 
 figure = plt.figure()
 ax = figure.gca(projection="3d")
