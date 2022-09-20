e587daf62de0d221b271f9d89a7feea0511f5115
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-09-20 14:48:47 +0200

    Commenting.

--- PartB.py
+++ PartB.py
@@ -1,4 +1,3 @@
-from cProfile import label
 from FrankeFunction import FrankeFunctionNoised
 from mean_square_error import MSE
 from r2_score import R2score
@@ -33,17 +32,21 @@ if __name__ == "__main__":
         X = create_design_matrix(x, y, current_polynominal)
         X_train, X_test, y_train, y_test = train_test_split(X, z.ravel(), test_size=test_size)
         
+        # Scaling of the data
         X_train = (X_train - np.mean(X_train))/np.std(X_train)
         X_test = (X_test - np.mean(X_test))/np.std(X_test)
         y_train = (y_train - np.mean(y_train))/np.std(y_train)
         y_test = (y_test - np.mean(y_test))/np.std(y_test)
 
+        # Using training data to create beta
         beta = calc_beta(X_train, y_train)
         beta_values.append(np.mean(beta))
 
+        # Using beta and test data to pretict y
         y_tilde_test = X_test @ beta
         y_tilde_train = X_train @ beta
 
+        # Calculating mean square error and R2 score for each polynomial
         mse_values_test.append(np.mean(MSE(y_test, y_tilde_test)))
         r2_score_values_test.append(np.mean(R2score(y_test, y_tilde_test)))
         mse_values_train.append(np.mean(MSE(y_train, y_tilde_train)))
@@ -52,13 +55,15 @@ if __name__ == "__main__":
     fig, axs = plt.subplots(2)
     fig.tight_layout(pad=5.0)
 
+    # Plotting mean square error for each polynomial
     axs[0].plot(np.arange(1, max_polynomial + 1, 1), mse_values_test, label="MSE test")
     axs[0].plot(np.arange(1, max_polynomial + 1, 1), mse_values_train, label="MSE train")
     axs[0].legend()
     axs[0].set_title("MSE")
     axs[0].set_xlabel("Polynomials")
     axs[0].set_ylabel("MSE")
-
+    
+    # Plotting R2 score for each polynomial
     axs[1].plot(np.arange(1, max_polynomial + 1, 1), r2_score_values_test, label="R2 score test")
     axs[1].plot(np.arange(1, max_polynomial + 1, 1), r2_score_values_train, label="R2 score train")
     axs[1].legend()
@@ -66,6 +71,7 @@ if __name__ == "__main__":
     axs[1].set_xlabel("Polynomials")
     axs[1].set_ylabel("R2 score")
 
+    # Plotting betas for each polynomial
     plt.figure()
     plt.plot(np.arange(1, max_polynomial + 1, 1), beta_values, label="Beta")
     plt.legend()
